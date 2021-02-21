1286f7f658bb12114cf85c229c1e23b9fdbdf9d5
Lena S <46871956+lsstenvi@users.noreply.github.com>
2021-02-21 02:34:10 -0500

    updated chat window: prettier

--- components/ChatViewComponent.js
+++ components/ChatViewComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, Image, View, ScrollView } from 'react-native';
-import { Item, Input, ListItem, CheckBox, Body } from "native-base";
+import { Item, Input, ListItem, CheckBox, Body, List, Left, Right, Icon } from "native-base";
 import Chatbot from 'react-chatbot-kit'
 import './App.css';
 import ActionProvider from './ActionProvider';
@@ -8,11 +8,31 @@ import MessageParser from './MessageParser';
 import config from './Config';
 
 
+const datas = [
+    "Mont Ripley",
+    "Nara Nature Park",
+    "Hungarian Falls Nature Area"
+  ];
+  
+
 class ChatViewComponent extends React.Component {
    render(){
     return (
       <div className="Chatbot">
         <header className="Chatbot-header">
+        <Text style={{fontSize: '12px', paddingBottom: '3px' }} > <b>Locations Relevant to you and Alex:</b></Text>
+        <List >
+            {datas.map((data, i) => (
+              <ListItem style={{height: '20px' }}>
+                <Left>
+                  <Text style={{fontSize: '10px' }}>{data}</Text>
+                </Left>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
+            ))}
+          </List>
         <Chatbot config={config} actionProvider={ActionProvider} 	    messageParser={MessageParser} />
         </header>
       </div>
