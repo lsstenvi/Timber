5d66bcab089a3d5220c2f5c874340ecd4b42e61c
Lena S <46871956+lsstenvi@users.noreply.github.com>
2021-02-21 01:36:13 -0500

    Changed the Avatar image

---
+++ components/BotAvatar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./BotAvatar.css";
+
+const BotAvatar = () => {
+    return (<div class="bot-avatar">A</div>)
+}
+
+export default BotAvatar;
--- components/Config.js
+++ components/Config.js
@@ -1,10 +1,16 @@
 
   
   // Config starter code
+  import React from 'react';
   import { createChatBotMessage } from "react-chatbot-kit";
+  import BotAvatar from "./BotAvatar";
   
   const config = {
-    initialMessages: [createChatBotMessage(`Hi there!`)]
+    botName: "Alex",
+    initialMessages: [createChatBotMessage(`Hi there!`)],
+    customComponents: { 
+      botAvatar: (props) => <BotAvatar {...props}/>
+    }
   }
   
   export default config
